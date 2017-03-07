720e061c4f21b5a37eebb28095c919bca17c531b
Herb Miller <herb@bobbingwide.com>
2017-03-07 15:15:41 +0000

    Fixes #5 - correct the test

--- tests/test-issue-5-disable-wp-custom-css.php
+++ tests/test-issue-5-disable-wp-custom-css.php
@@ -1,6 +1,5 @@
 <?php // (C) Copyright Bobbing Wide 2017
 
-
 /**
  * Confirm that wp_custom_css_cb is not registered as a filter function for wp_head
  *
@@ -10,14 +9,19 @@
 class Tests_issue_5_disable_wp_custom_css extends BW_UnitTestCase {
 
 	/**
+	 * Tests that 'wp_custom_css_cb' is no longer registered to 'wp_head'
 	 * 
-	 * Finds the name of the functions.php file
-	 * `C:\apache\htdocs\wordpress\wp-content\themes\genesis-oik/functions.php`
-	 * with \ converted to /
+	 * Regardless of the priority.
+	 * 
+	 * In the first implementation of this test the function name was wrong, so it passed invalidly. 
+	 * Now we check the function is defined, belt and braces.
+	 *
 	 */
 	function test_wp_custom_css_cb_not_attached_to_wp_head_action() {
-	
-		$has_filter = has_filter( "wp_head", "wp_customcsss_cb" );
+		$function = "wp_custom_css_cb";
+		$exists = function_exists( $function  );
+		$this->assertTrue( $exists );
+		$has_filter = has_filter( "wp_head", $function );
 		$this->assertFalse( $has_filter );
 	}
 	
